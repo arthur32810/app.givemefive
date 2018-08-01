b7df3bc1bca94bdfa71db05f6b114176bded5789
arthur robert <arthur32810@gmail.com>
2018-08-01 15:09:16 +0200

    mise en place du formulaire

--- src/GMF/DevisBundle/Form/ContactType.php
+++ src/GMF/DevisBundle/Form/ContactType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,15 +18,16 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('surname', TextType::class)
-            ->add('name',    TextType::class)
-            ->add('company', TextType::class)
-            ->add('siret',   TextType::class)
+            ->add('surname', TextType::class, array('label' => 'Nom'))
+            ->add('name',    TextType::class, array('label'  => 'Prénom'))
+            ->add('company', TextType::class, array('required' => false))
+            ->add('siret',   TextType::class, array('required' => false))
             ->add('adress',  TextType::class)
             ->add('zipCode', IntegerType::class)
             ->add('city',    TextType::class)
             ->add('email',   EmailType::class)
-            ->add('phone',   IntegerType::class);
+            ->add('phone',   IntegerType::class)
+            ->add('envoyer', SubmitType::class);
     }/**
      * {@inheritdoc}
      */
