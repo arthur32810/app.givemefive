e0728923bbe1f69c8c0971862ada3076f667b746
arthur robert <arthur32810@gmail.com>
2018-08-18 09:20:26 +0200

    design du formulaire

--- src/GMF/DevisBundle/Form/ContactType.php
+++ src/GMF/DevisBundle/Form/ContactType.php
@@ -19,13 +19,13 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('surname', TextType::class, array('label' => 'Nom'))
-            ->add('name',    TextType::class, array('label'  => 'Prénom'))
+            ->add('surname', TextType::class, array('label' => 'Nom', 'attr'=>array('placeholder' => 'Votre nom')))
+            ->add('name',    TextType::class, array('label'  => 'Prénom', 'attr'=>array('placeholder'=> 'Votre prénom')))
             ->add('company', TextType::class, array('required' => false, 'attr'=>array('placeholder'=> 'Votre entreprise')))
             ->add('siret',   TextType::class, array('required' => false, 'attr'=>array('placeholder'=>'Siret de votre entreprise')))
-            ->add('adress',  TextType::class)
-            ->add('zipCode', IntegerType::class)
-            ->add('city',    TextType::class)
+            ->add('adress',  TextType::class, array('attr'=>array('placeholder'=>"numero, rue")))
+            ->add('zipCode', IntegerType::class, array('attr'=>array('placeholder'=>"Code Postal")))
+            ->add('city',    TextType::class, array('attr'=>array('placeholder'=>"Ville")))
             ->add('email',   EmailType::class, array('attr'=>array('placeholder' => 'Votre e-mail')))
             ->add('email2',  EmailType::class, array('attr'=>array('placeholder' => 'Confirmer votre adresse e-mail')))
             ->add('phone',   TextType::class, array('attr' =>array('placeholder' => '09.23.48.87.00')))
