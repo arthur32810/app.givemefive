e6ea2583716001b8355be4b4e8b26ee490bbea02
arthur robert <arthur32810@gmail.com>
2018-08-19 16:39:19 +0200

    mise en forme de l'administration

--- src/GMF/AdminBundle/Controller/AdminController.php
+++ src/GMF/AdminBundle/Controller/AdminController.php
@@ -62,18 +62,25 @@ class AdminController extends Controller
 
     public function usersAction()
     {
-    	$em = $this
-		  ->getDoctrine()
-		  ->getManager();
+    	$userManager = $this->get('fos_user.user_manager');
 
-		$list_users = $em->getRepository('GMFUserBundle:User')->findAll();
+		$list_users = $userManager->findUsers();
 
 		return $this->render('GMFAdminBundle:Admin:users.html.twig', array('list_users'=>$list_users));
     }
 
-    public function user_editAction()
+    public function user_editAction($username)
     {
-    	return new Response('edit');
+    	$userManager = $this->get('fos_user.user_manager');
+
+    	$user = $userManager->findUserBy(array('username'=>$username));
+    	$user->setRoles('ROLE_EDITEUR');
+    	$userManager->upateUser($user);
+
+    	$session = $request->getSession();
+		$session->getFlashBag()->add('success', 'L\'utilisateur a bien été modifié');
+
+		return $this->redirectToRoute('gmf_admin_users');
     }
 
     public function delete_userAction(Request $request, $username)
