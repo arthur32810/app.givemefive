83b83fda67c24450f7f1f9ded9f83aa2d534f658
arthur robert <arthur32810@gmail.com>
2018-08-22 14:17:37 +0200

    création de l'entité Modules

---
+++ src/GMF/DevisBundle/Entity/Modules.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace GMF\DevisBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Modules
+ *
+ * @ORM\Table(name="modules")
+ * @ORM\Entity(repositoryClass="GMF\DevisBundle\Repository\ModulesRepository")
+ */
+class Modules
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="price", type="decimal", precision=3, scale=2)
+     */
+    private $price;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set type.
+     *
+     * @param string $type
+     *
+     * @return Modules
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type.
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set name.
+     *
+     * @param string $name
+     *
+     * @return Modules
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description.
+     *
+     * @param string $description
+     *
+     * @return Modules
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description.
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set price.
+     *
+     * @param string $price
+     *
+     * @return Modules
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price.
+     *
+     * @return string
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
